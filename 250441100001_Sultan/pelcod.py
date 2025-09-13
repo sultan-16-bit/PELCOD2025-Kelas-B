65c9d57ab41953c4d769017876951468f8ace229
sultan-16-bit <sultanyuni05@gmail.com>
2025-09-13 11:37:33 +0700

    250441100001_Sultan

---
+++ 250441100001_Sultan/pelcod.py
@@ -0,0 +1,30 @@
+# nama = "Sultan" 
+# nim = 250441100001
+# semester = 1
+
+# print("=====BIODATA MAHASISWA=====")
+# print("Halo, nama saya:"+ nama + "\n")
+# print(f"NIM saya : {nim}\n")
+# print("saya berkuliah di semester : {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b # penjumlahan
+# d = a - b # pengurangan
+# e = a * b # perkalian
+# f = a / b # pembagian
+# g = a % b # sisa bagi
+# print("Hasil penjumlahan:", c)
+# print("Hasil pengurangan:", d)
+# print("Hasil perkalian:", e)
+# print("Hasil pembagian:", f)
+# print("Hasil sisa bagi:", g)
+
+nama = input ("masukkan nama anda: ")
+umur = input("masukkan umur anda: ")
+buah = input("masukkan buah favorit anda: ")
+
+print("masukkan nama anda " + nama)
+print("masukkan umur anda " + umur + "tahun")
+print("masukkan buah favorit anda " + buah)
+
