02d2b662d604b0b997856409246b3e3d4e5101c4
sultan-16-bit <sultanyuni05@gmail.com>
2025-09-19 13:18:31 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,19 @@
+tugas = float(input ("Masukkan Nilai Tugas: "))
+uts = float (input ("Masukkan Nilai uts: "))
+uas = float (input("Masukkan Nilai uas: "))
+
+nilai_tugas = 0.30
+nilai_uts = 0.30
+nilai_uas = 0.40
+
+print ()
+
+print ("rata rata nilai_tugas:", nilai_tugas)
+print ("rata rata nilai_uts:", nilai_uts)
+print ("rata rata nilai_uas:", nilai_uas)
+
+print ()
+
+nilai_akhir = (tugas * nilai_tugas) + (uts * nilai_uts) + (uas * nilai_uas)
+
+print("Nilai Akhir Andi Adalah:", nilai_akhir)
